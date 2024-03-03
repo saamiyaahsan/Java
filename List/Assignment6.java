2b92dfb9ce6c6326b4819fdbfcf4a19f92a98463
Saamiya Ahsan <saamiyaa835@gmail.com>
2024-03-03 19:34:39 +0530

    Create Assignment6.java

---
+++ List/Assignment6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Assignment6{
+     
+     public static void main(String[] args){
+          
+          Vector<String>list = new Vector<>();
+          list.add("A");
+          list.add("B");
+          list.add("C");
+          list.add("D");
+          list.add("E");
+          list.add("F");
+
+          System.out.println("The List is "+list);
+     }
+}
