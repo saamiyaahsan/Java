a9a1300725aa3b2fbd5850884f516777c63da750
Saamiya Ahsan <saamiyaa835@gmail.com>
2024-03-03 19:29:30 +0530

    Create Assignment1.java

---
+++ List/Assignment1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Assignment{
+     
+     public static void main(String[] args){
+
+         ArrayList<String> list1 = new ArrayList<>();
+
+         list1.add("January");
+         list1.add("February");
+         list1.add("March");
+         list1.add("April");
+         list1.add("May");
+         list1.add("June");
+         list1.add("July");
+         list1.add("August");
+         list1.add("September");
+         list1.add("October");
+         list1.add("November");
+         list1.add("December");
+
+         System.out.println("The list is "+list1); 
+     }
+}
