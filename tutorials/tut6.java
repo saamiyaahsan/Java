9d607243dbbe0bcb398718381dc5b07c5d5288ec
Saamiya Ahsan <saamiyaa835@gmail.com>
2024-03-09 02:40:31 +0530

    Create tut6.java

---
+++ tutorials/tut6.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+class MyException extends Exception{
+     
+     @Override
+     public String toString(){
+         return "I am toString()";
+     }
+
+     @Override
+     public String getMessage(){
+         return "I am getMessage()";
+     }
+}
+
+class MaxAgeException extends Exception{
+     
+     @Override
+     public String toString(){
+          
+          return "Age cannot be greater than 129";
+     }
+
+     @Override
+     public String getMessage(){
+         return "Make sure that the value of entered age is correct";
+     }
+}
+
+public class helper{
+     
+     public static void main(String[] args){
+          
+          Scanner sc = new Scanner(System.in);
+          int a = sc.nextInt();
+
+          if(a < 9){
+               
+               try{
+                    throw new ArithmeticException("This is an Exception");
+               }
+
+               catch(Exception e){
+                    System.out.println(e.getMessage());
+                    System.out.println(e.toString());
+                    e.printStackTrace();
+                    System.out.println("Finished");
+               }
+
+               System.out.println("Yes finished");
+          }
+     }
+}
