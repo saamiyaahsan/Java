5ab2b477fb849bd5689d53c8b9b7acef65309d73
Saamiya Ahsan <saamiyaa835@gmail.com>
2024-03-09 02:48:46 +0530

    Create tut13.java

---
+++ tutorials/tut13.java
@@ -0,0 +1,10 @@
+class ThreadExample{
+      
+      public static void main(String[] args){
+           
+           func1();
+           func2();
+
+           // func2() will be called after func1() due to the absence of thread
+      }
+}
