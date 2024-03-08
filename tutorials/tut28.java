6fb3dfcc9f8261e26e780539b4f99dd5c521062b
Saamiya Ahsan <saamiyaa835@gmail.com>
2024-03-09 03:03:47 +0530

    Create tut28.java

---
+++ tutorials/tut28.java
@@ -0,0 +1,37 @@
+// Method overriding
+
+class A{
+     public int a;
+
+     public int harry(){
+          return 4;
+     }
+
+     public void meth2(){
+         System.out.println("I am a method of class A");
+     }
+}
+
+class B extends A{
+     
+     @Override
+     public void meth2(){
+         System.out.println("I am a method2 of class B");
+     }
+
+     public void meth3(){
+         System.out.println("I am a method3 of class B");
+     }
+}
+
+public class helper{
+     
+     public static void main(String[] args){ 
+
+         A a = new A();
+         a.meth2();
+
+         B b = new B();
+         b.meth2();
+     }
+}
