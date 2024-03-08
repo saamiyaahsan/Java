dface955fde8333e5e7fb38becc8b8995367edf3
Saamiya Ahsan <saamiyaa835@gmail.com>
2024-03-09 02:38:21 +0530

    Create tut3.java

---
+++ tutorials/tut3.java
@@ -0,0 +1,8 @@
+
+public class helper{
+     
+     public static void main(String[] args){
+          System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+          System.out.println(System.currentTimeMillis());
+     }
+}
