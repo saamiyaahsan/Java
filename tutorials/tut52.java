fd3a6eb28597c310bf283baba1e0d7111dfeb9f7
Saamiya Ahsan <saamiyaa835@gmail.com>
2024-03-09 11:57:28 +0530

    Create tut52.java

---
+++ tutorials/tut52.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class tut2{
+
+    public static void main(String[] args){
+        int a = 20;
+
+        if(a > 20){
+            System.out.println("Yes");
+        }
+
+        else{
+            System.out.println("No");
+        }
+    }
+}
