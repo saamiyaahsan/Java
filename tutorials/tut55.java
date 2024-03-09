5e295e46c6ed490e9118ec8e6ec29ee8465039ea
Saamiya Ahsan <saamiyaa835@gmail.com>
2024-03-09 12:00:39 +0530

    Create tut55.java

---
+++ tutorials/tut55.java
@@ -0,0 +1,19 @@
+public class tut5{
+
+    public static void main(String[] args){
+
+        int i=10;
+
+        while(i > 0){
+            System.out.println(i);
+            i--;
+        }
+
+        i = 100;
+
+        while(i <= 200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
